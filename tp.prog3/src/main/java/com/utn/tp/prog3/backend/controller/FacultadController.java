becffdb65ca2ba106ce0d21d88d00f625f1a7da7
adlfvrr <adolfover4@gmail.com>
2026-06-15 17:50:49 -0300

    feat: Implementación de controlador de facultades para el funcionamiento de la vista

---
+++ tp.prog3/src/main/java/com/utn/tp/prog3/backend/controller/FacultadController.java
@@ -0,0 +1,55 @@
+package com.utn.tp.prog3.backend.controller;
+
+import com.utn.tp.prog3.backend.dto.request.AddFacultadRequest;
+import com.utn.tp.prog3.backend.dto.request.UpdateFacultadRequest;
+import com.utn.tp.prog3.backend.dto.response.FacultadResponse;
+import com.utn.tp.prog3.backend.service.Iservices.IFacultadService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tp/facultades")
+@AllArgsConstructor
+public class FacultadController {
+
+    private final IFacultadService facultadService;
+
+    @GetMapping
+    public ResponseEntity<Page<FacultadResponse>> obtenerTodasFacultades(@RequestParam(required = false) String nombre,
+                                                                         @RequestParam(required = false) String direccion,
+                                                                         @RequestParam(required = false) String cuit,
+                                                                         @RequestParam(required = false) String telefono,
+                                                                         @RequestParam(required = false) String email,
+                                                                         @PageableDefault(size = 5) Pageable pageable) {
+        return ResponseEntity.ok(facultadService.findAll(nombre, direccion, cuit, telefono, email, pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FacultadResponse> obtenerFacultadPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(facultadService.findById(id));
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')") // Solo usuarios con rol ADMIN pueden crear terceros
+    public ResponseEntity<FacultadResponse> crearFacultad(@RequestBody AddFacultadRequest request) {
+        return ResponseEntity.ok(facultadService.addFacultad(request));
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<FacultadResponse> actualizarFacultad(@RequestBody UpdateFacultadRequest request, @PathVariable Long id) {
+        return ResponseEntity.ok(facultadService.updateFacultad(request, id));
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void borrarFacultad(@PathVariable Long id) {
+        facultadService.deleteFacultad(id);
+    }
+
+}
