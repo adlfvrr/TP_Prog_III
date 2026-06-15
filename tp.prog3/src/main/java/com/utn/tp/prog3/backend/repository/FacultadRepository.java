7c8679636739cd529dcdebb3c8c1f30f20e2c1c8
adlfvrr <adolfover4@gmail.com>
2026-06-15 17:48:25 -0300

    feat: Implementación de métodos para el filtrado en el repositorio de facultades

--- tp.prog3/src/main/java/com/utn/tp/prog3/backend/repository/FacultadRepository.java
+++ tp.prog3/src/main/java/com/utn/tp/prog3/backend/repository/FacultadRepository.java
@@ -1,11 +1,19 @@
 package com.utn.tp.prog3.backend.repository;
 
 import com.utn.tp.prog3.backend.model.Facultad;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface FacultadRepository extends JpaRepository<Facultad, Long> {
+
+    Page<Facultad> findByNombreContainingIgnoreCase(String nombre, Pageable pageable);
+    Page<Facultad> findByDireccionContainingIgnoreCase(String direccion, Pageable pageable);
+    Page<Facultad> findByCuitContainingIgnoreCase(String cuit, Pageable pageable);
+    Page<Facultad> findByTelefonoContainingIgnoreCase(String telefono, Pageable pageable);
+    Page<Facultad> findByEmailContainingIgnoreCase(String email, Pageable pageable);
 
     Optional<Facultad> findByCuit(String cuit);
 
