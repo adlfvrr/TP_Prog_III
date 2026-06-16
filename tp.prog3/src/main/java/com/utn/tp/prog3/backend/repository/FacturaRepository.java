6b47d7510fc99f4dba58bfe173574f2e5c41c0ac
adlfvrr <adolfover4@gmail.com>
2026-06-16 10:21:18 -0300

    feat: Implementando métodos al repositorio para el filtrado de facturas

--- tp.prog3/src/main/java/com/utn/tp/prog3/backend/repository/FacturaRepository.java
+++ tp.prog3/src/main/java/com/utn/tp/prog3/backend/repository/FacturaRepository.java
@@ -1,11 +1,18 @@
 package com.utn.tp.prog3.backend.repository;
 
 import com.utn.tp.prog3.backend.model.Factura;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.Optional;
 
 public interface FacturaRepository extends JpaRepository<Factura, Long> {
+
+    Page<Factura> findByNumero(Integer numero, Pageable pageable);
+    Page<Factura> findByTerceroCuit(String cuit, Pageable pageable);
+    Page<Factura> findByFechaFactura(Date fecha_Factura, Pageable pageable);
 
     Optional<Factura> findByNumero(int numero);
 }
