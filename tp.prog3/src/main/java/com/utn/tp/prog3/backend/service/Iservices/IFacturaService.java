6aca04940d9c5fd66c2020ff122bf6fde10ff9cc
adlfvrr <adolfover4@gmail.com>
2026-06-16 10:23:06 -0300

    fix: Arreglo de métodos para el filtrado. del: Eliminación de métodos ya no necesarios

--- tp.prog3/src/main/java/com/utn/tp/prog3/backend/service/Iservices/IFacturaService.java
+++ tp.prog3/src/main/java/com/utn/tp/prog3/backend/service/Iservices/IFacturaService.java
@@ -2,17 +2,16 @@ package com.utn.tp.prog3.backend.service.Iservices;
 
 import com.utn.tp.prog3.backend.dto.request.AddFacturaRequest;
 import com.utn.tp.prog3.backend.dto.response.CompleteFacturaResponse;
-import com.utn.tp.prog3.backend.dto.response.FacturaResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+import java.util.Date;
 
 public interface IFacturaService {
 
-    List<FacturaResponse> findAll();
-    List<CompleteFacturaResponse> findAllComplete();
+    Page<CompleteFacturaResponse> findAllComplete(Integer numero, String cuit, Date fechaFactura, Pageable pageable);
     CompleteFacturaResponse findByIdComplete(Long id);
-    FacturaResponse findById(Long id);
-    FacturaResponse addFactura(AddFacturaRequest request);
+    CompleteFacturaResponse addFactura(AddFacturaRequest request);
     // No tiene sentido un update de facturas, puesto que en la cotidianidad una factura mal emitida directamente se anula, no se modifica
     void deleteFactura(Long id);
 
