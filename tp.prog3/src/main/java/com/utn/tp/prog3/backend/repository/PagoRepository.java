b5a076ab7d6ffec1a7edbc899d40df73d52ce017
adlfvrr <adolfover4@gmail.com>
2026-06-17 00:35:40 -0300

    feat: Implementación de métodos en PagoRepository para el filtrado de Pagos

--- tp.prog3/src/main/java/com/utn/tp/prog3/backend/repository/PagoRepository.java
+++ tp.prog3/src/main/java/com/utn/tp/prog3/backend/repository/PagoRepository.java
@@ -1,9 +1,16 @@
 package com.utn.tp.prog3.backend.repository;
 
 import com.utn.tp.prog3.backend.model.Pago;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
 
 public interface PagoRepository extends JpaRepository<Pago, Long> {
 
+    Page<Pago> findByTerceroCuitContainingIgnoreCase(String cuit, Pageable pageable);
+    Page<Pago> findByModoPagoContainingIgnoreCase(String modoPago, Pageable pageable);
+    Page<Pago> findByFechaPago(Date fechaPago, Pageable pageable);
 
 }
